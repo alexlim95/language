51505a84fda06240acd4a60f9f2d6692d50b9723
Alex Lim <31427206+alexlim95@users.noreply.github.com>
2018-03-11 19:03:49 -0700

    Update fact-while.c

--- ia32-x86/fact-while.c
+++ ia32-x86/fact-while.c
@@ -1,3 +1,11 @@
+/*
+*  Function to observe when compiling for IA32 and x86-64.
+*  
+*  Using GCC:
+*  Compile for IA32: unix> gcc -O1 -S -m32 fact-while.c -o fact-while-32 -fomit-frame-pointer
+*  Compile for x86-64: unix> gcc -O1 -S -m64 fact-while.c -o fact-while-64 -fomit-frame-pointer
+*/
+
 int fact_while(int n)
 {
   int result = 1;
